28504c024dc62676211d28987e74a82ecbf336a0
Enzo Lespart <enzolespart@gmail.com>
2018-05-17 19:17:53 +0100

    commit inicial

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as Phaser from 'phaser-ce';
+
+
+window.onload = function() {
+
+    // Configuration object
+    var config = {
+        width: 600,
+        height: 800,
+        renderer: Phaser.AUTO,
+        antialias: false,
+        multiTexture: true,
+        state: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    }
+    // Game initialization
+    var game = new Phaser.Game(config);
+
+    function preload () {
+        
+    }
+
+    function create () {
+
+    }
+
+    function update () {
+
+    }
+
+}; 
