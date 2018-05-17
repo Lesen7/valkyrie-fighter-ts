1f48d71f3ad159ead268fba583e45688b1fb4e99
Enzo Lespart <enzolespart@gmail.com>
2018-05-17 20:14:18 +0100

    estructura de classes básica

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,52 @@
 import * as Phaser from 'phaser-ce';
 
+// Class imports
+import { Actor } from './models/actor';
+import { Player } from './models/player';
+import { Enemy } from './models/enemy';
 
 window.onload = function() {
-
     // Configuration object
     var config = {
         width: 600,
         height: 800,
         renderer: Phaser.AUTO,
+        transparent: false,
         antialias: false,
         multiTexture: true,
         state: {
             preload: preload,
             create: create,
             update: update
+        },
+        physicsConfig: {
+
         }
     }
     // Game initialization
     var game = new Phaser.Game(config);
 
     function preload () {
-
+        // Background images
+        game.load.image('stars_bg_0', 'assets/backgrounds/stars_bg_0.png');
+        game.load.image('stars_bg_1', 'assets/backgrounds/stars_bg_1.png');
+    
+        // Player sprites/images
+        game.load.image('player_bullet', 'assets/sprites/vf1/player_bullet.png');
+        game.load.spritesheet('vf1_sp', 'assets/sprites/vf1/vf1_sp.png', 32, 37);
+        game.load.spritesheet('vf1_turn_l', 'assets/sprites/vf1/vf1_turn_l.png', 32, 37);
+        game.load.spritesheet('vf1_turn_r', 'assets/sprites/vf1/vf1_turn_r.png', 32, 37);
+    
+        // Enemy sprites/images
+        game.load.spritesheet('reguld_sp', 'assets/sprites/reguld/reguld_sp.png', 32, 37);
+        game.load.spritesheet('reguld_move', 'assets/sprites/reguld/reguld_move.png', 32, 37);
     }
 
-    function create () {
+    // Player variables
+    var player;
 
+    function create () {
+        player = game.add.sprite(32, game.world.height - 150, 'vf1_sp');
     }
 
     function update () {
