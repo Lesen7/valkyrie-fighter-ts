bbda7c02bf6f2158fb69c4cc2c93a9b0223b047a
Enzo Lespart <enzolespart@gmail.com>
2018-05-18 15:21:23 +0100

    clase actor actualizada

--- src/models/actor.ts
+++ src/models/actor.ts
@@ -2,6 +2,8 @@ import { Sprite } from "phaser-ce";
 
 export default interface Actor {
     sprite: Sprite;
+    xPosition: number;
+    yPosition: number;
     health: number;
     speed: number;
 }
