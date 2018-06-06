dfa6590800f11e2d89628c725674d9e52f7afef2
Enzo Lespart <enzolespart@gmail.com>
2018-06-06 19:23:44 +0100

    documented and slightly refactored the SpawnPoint class

--- src/models/spawnPoint.ts
+++ src/models/spawnPoint.ts
@@ -7,14 +7,27 @@ import gameMaster from '../index';
 import GameMaster from "./gameMaster";
 
 export default class SpawnPoint extends Actor {
-    // Properties
     sprite: Sprite;
+    /**
+     * The frequency at which enemies will spawn.
+     */
     frequency: number;
-    top: number;
-    counter: number;
-    gameMaster: GameMaster;
+    /**
+     * The maximum amount the counter must reach.
+     */
+    private top: number;
+    /**
+     * The value that will count up the frequency.
+     */
+    private counter: number;
 
-    // Methods
+    /**
+     * An object that spawns enemies at a certain rate. Usually located out of bounds.
+     * @param game The Phaser game the sprite will be added to.
+     * @param gameMaster The Game Master the object will be added to.
+     * @param sprite The sprite for the object.
+     * @param frequency The frequency at which enemies will spawn.
+     */
     constructor(game: Game, gameMaster, sprite: Sprite, frequency: number) {
         super(game, sprite);
         this.gameMaster = gameMaster;
@@ -24,21 +37,32 @@ export default class SpawnPoint extends Actor {
         this.counter = 0;
     }
 
+    /**
+     * Rolls a number between 0 and 0.9.
+     * If the result is less than 0.6, resets the spawn counter.
+     * If it's greater or equal, spanws an enemy.
+     */
     roll() {
         let result = Math.random();
         if(result < 0.6) {
             this.counter = 0;
             return;
-        } else if(result > 0.6) {
+        } else if(result >= 0.6) {
             this.spawn();
         }
     }
 
+    /**
+     * Spawns an enemy and resets the spawn counter.
+     */
     spawn() {
         this.counter = 0;
         gameMaster.enemies.push(new Pod(this.game, this.gameMaster, this.game.add.sprite(this.sprite.x, this.sprite.y, 'pod_move')));
     }
 
+    /**
+     * Counts the counter up by the spawn frequency and rolls to see whether it should spawn or not when the counter reaches the top.
+     */
     update() {
         if (this.counter >= this.top) {
             this.roll();
